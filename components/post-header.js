483565d8d670fff6d835ca7bc41a2490b97d5a35
thasup <thanachonfirst@hotmail.com>
2023-07-11 03:29:19 +0700

    Relocate date time

--- components/post-header.js
+++ components/post-header.js
@@ -7,8 +7,13 @@ export default function PostHeader({ title, coverImage, date, author }) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">
-        {author && <Avatar name={author.name} picture={author.picture} />}
+      <div className="flex flex-row justify-between	items-start">
+        <div className="hidden md:block md:mb-12">
+          {author && <Avatar name={author.name} picture={author.picture} />}
+        </div>
+        <div className="mb-6 text-lg">
+          <DateComponent dateString={date} />
+        </div>
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} url={coverImage.url} />
@@ -16,9 +21,6 @@ export default function PostHeader({ title, coverImage, date, author }) {
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
           {author && <Avatar name={author.name} picture={author.picture} />}
-        </div>
-        <div className="mb-6 text-lg">
-          <DateComponent dateString={date} />
         </div>
       </div>
     </>
