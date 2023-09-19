68bdbc47a2da361deb61448d4377a8fef5dbb4db
thasup <thanachonfirst@hotmail.com>
2023-09-19 09:56:56 +0700

    add google analytics 4

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      {/* Google tag (gtag.js) */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-LV89KM89EE"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', 'G-LV89KM89EE');
+      </script>
       <body>
         <Main />
         <NextScript />
